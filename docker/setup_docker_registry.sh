3c7bee0cc5ad93072677cb36b044057738b827cf
Omar Cruz Pantoja <opantoja3@gatech.edu>
2025-07-19 18:08:23 -0400

    replace cors config

--- docker/setup_docker_registry.sh
+++ docker/setup_docker_registry.sh
@@ -2,25 +2,8 @@
 
 set -e
 
-echo "📄 Writing registry-config.yml..."
 mkdir -p /opt/docker-registry
 
-cat > /opt/docker-registry/registry-config.yml <<EOF
-version: 0.1
-log:
-  fields:
-    service: registry
-
-http:
-  addr: :5000
-  headers:
-    Access-Control-Allow-Origin: ["http://192.168.0.22:9444"]
-    Access-Control-Allow-Methods: ["GET", "HEAD", "OPTIONS", "DELETE"]
-    Access-Control-Allow-Headers: ["Authorization", "Accept", "Cache-Control"]
-storage:
-  filesystem:
-    rootdirectory: /var/lib/registry
-EOF
 
 echo "🚀 Starting Docker Compose..."
 docker compose -f docker_registry_compose.yml up -d
